83dd5bd9da906f4ccfff6b22352fe881ff5e20ba
tg666 <tomasglawaty@icloud.com>
2020-11-20 04:41:44 +0100

    Nette 3 support and 68publishers/translation-bridge
    - changed nette dependendies to version `3.0`
    - removed dependency and suggestions for `kdyby/translation`
    - added dev dependency and suggestion for `68publishers/translation-bridge`
    - refactored CompilerExtension
    - removed class `ProfileStorageResolver`
    - added class `TranslatorLocaleResolver`
    - modified tests for Nette 3 and new integration
    - updated README

--- tests/Helper/ContainerFactory.php
+++ tests/Helper/ContainerFactory.php
@@ -4,57 +4,26 @@ declare(strict_types=1);
 
 namespace SixtyEightPublishers\i18n\Tests\Helper;
 
-use Tester\FileMock;
-use Nette\DI\Compiler;
+use Nette\Configurator;
 use Nette\DI\Container;
-use Nette\DI\ContainerLoader;
-use Nette\Bridges\HttpDI\HttpExtension;
-use Tracy\Bridges\Nette\TracyExtension;
-use Nette\Bridges\HttpDI\SessionExtension;
-use Kdyby\Translation\DI\TranslationExtension;
-use SixtyEightPublishers\i18n\DI\I18nExtension;
 
 final class ContainerFactory
 {
 	/**
-	 * @param string       $name
-	 * @param string|array $config
+	 * @param string|array|NULL $config
 	 *
 	 * @return \Nette\DI\Container
 	 */
-	public static function createContainer(string $name, $config): Container
+	public static function createContainer($config = NULL): Container
 	{
-		if (!defined('TEMP_PATH')) {
-			define('TEMP_PATH', __DIR__ . '/../temp');
+		$configurator = new Configurator();
+
+		$configurator->setTempDirectory(TEMP_PATH);
+
+		if (NULL !== $config) {
+			$configurator->addConfig($config);
 		}
 
-		$loader = new ContainerLoader(TEMP_PATH . '/cache/Nette.Configurator', TRUE);
-		$class = $loader->load(static function (Compiler $compiler) use ($config): void {
-			$compiler->addExtension('translation', new TranslationExtension());
-			$compiler->addExtension('tracy', new TracyExtension());
-			$compiler->addExtension('http', new HttpExtension());
-			$compiler->addExtension('session', new SessionExtension());
-			$compiler->addExtension('i18n', new I18nExtension());
-
-			$compiler->addConfig([
-				'parameters' => [
-					'appDir' => __DIR__ . '/../../src',
-					'tempDir' => __DIR__ . '/../temp',
-					'wwwDir' => __DIR__ . '/../temp/temp',
-					'filesDir' => __DIR__ . '/../files',
-					'debugMode' => FALSE,
-				],
-			]);
-
-			if (is_array($config)) {
-				$compiler->addConfig($config);
-			} elseif (is_string($config) && is_file($config)) {
-				$compiler->loadConfig($config);
-			} elseif (NULL !== $config) {
-				$compiler->loadConfig(FileMock::create((string) $config, 'neon'));
-			}
-		}, $name);
-
-		return new $class();
+		return $configurator->createContainer();
 	}
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,3 +8,11 @@ if (@!include __DIR__ . '/../vendor/autoload.php') {
 }
 
 Tester\Environment::setup();
+
+if (!defined('TEMP_PATH')) {
+	define('TEMP_PATH', __DIR__ . '/temp');
+}
+
+if (!defined('CONFIG_DIR')) {
+	define('CONFIG_DIR', __DIR__ . '/config');
+}
